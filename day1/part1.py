fddca04c41fa3d202e5224fe1359a3acbe53c3e3
dylan robins <dylan.robins@gmail.com>
2023-12-01 18:28:09 +0100

    Reformatted using black

--- day1/part1.py
+++ day1/part1.py
@@ -1,52 +1,49 @@
 from pathlib import Path
 from typing import Iterable
 
+
 def get_first_digit(s: str) -> str:
     for char in s:
         if char.isdigit():
             return char
     raise ValueError(f"No digit was found in the string '{s}'")
 
+
 def sum_calibration_values(lines: Iterable[str]) -> int:
     sum = 0
     for line in lines:
-        calibration_digits = (
-            get_first_digit(line)
-            + get_first_digit(line[::-1])
-        )
+        calibration_digits = get_first_digit(line) + get_first_digit(line[::-1])
         sum += int(calibration_digits)
-    
+
     return sum
-    
+
+
 def test():
-    example_data = (
-        "1abc2\n"
-        "pqr3stu8vwx\n"
-        "a1b2c3d4e5f\n"
-        "treb7uchet\n"
-    )
+    example_data = "1abc2\n" "pqr3stu8vwx\n" "a1b2c3d4e5f\n" "treb7uchet\n"
     print("Example data:")
-    print(example_data, end='')
-    
+    print(example_data, end="")
+
     lines = example_data.splitlines()
     result = sum_calibration_values(lines)
     expected = 142
     print(f"{result=} | {expected=}")
-    
+
     if result == expected:
         print("TEST PASSED")
     else:
         print("TEST FAILED")
         exit(1)
-    print("_"*80)
-    
+    print("_" * 80)
+
+
 def main():
     input_file = Path(__file__).parent / "input.txt"
     print(f"Real data from {input_file}:")
     lines = input_file.read_text().splitlines()
     print("Result:", sum_calibration_values(lines))
-    print("_"*80)
-    
+    print("_" * 80)
+
+
 if __name__ == "__main__":
     test()
     main()
--- day1/part2.py
+++ day1/part2.py
@@ -15,6 +15,7 @@ _ALL_DIGITS = [
     "nine",
 ]
 
+
 class MatchedDigit(NamedTuple):
     index: int
     digit: str
@@ -24,46 +25,46 @@ def debug(*args, indent=0, **kwargs):
     if _DEBUG:
         prefix = "### Debug:" + "    " * indent
         print(prefix, *args, **kwargs)
-        
+
 
 def get_first_numeric_digit(s: str, reverse: bool = False) -> MatchedDigit | None:
     if reverse:
         s = s[::-1]
-    
+
     for index, char in enumerate(s):
         if char.isdigit():
             return MatchedDigit(index, char)
-    
+
     return None
 
 
 def get_first_text_digit(s: str, reverse: bool = False) -> MatchedDigit | None:
     if reverse:
         s = s[::-1]
-    
+
     matches = []
-    
+
     for digit, substring in enumerate(_ALL_DIGITS, start=1):
         if reverse:
             substring = substring[::-1]
-            
+
         debug(f"Looking for {substring} in {s}", indent=1)
-        
+
         if (pos := s.find(substring)) != -1:
             matches.append(MatchedDigit(pos, str(digit)))
-    
+
     if not matches:
         return None
     return min(matches, key=lambda m: m.index)
 
+
 def get_calibration_value(line: str, which: Literal["start", "end"] = "start") -> str:
-    
     reverse = which == "end"
     debug(f"Processing line {line} ({reverse=})")
-    
+
     start_numeric_match = get_first_numeric_digit(line, reverse)
     start_text_match = get_first_text_digit(line, reverse)
-    
+
     # If both are found: compare indices to find first
     if start_numeric_match is not None and start_text_match is not None:
         if start_numeric_match.index < start_text_match.index:
@@ -78,7 +79,7 @@ def get_calibration_value(line: str, which: Literal["start", "end"] = "start") -
         return start_text_match.digit
     else:
         raise ValueError(f"No digit was found in the line '{line}'")
-    
+
 
 def sum_calibration_values(lines: Iterable[str]) -> int:
     sum = 0
@@ -88,16 +89,16 @@ def sum_calibration_values(lines: Iterable[str]) -> int:
         debug(f"{line}: {first=} ... {last=}")
         sum += int(first + last)
     return sum
-    
-    
+
+
 def main():
     input_file = Path(__file__).parent / "input.txt"
     print(f"Real data from {input_file}:")
     lines = input_file.read_text().splitlines()
     print("Result:", sum_calibration_values(lines))
-    print("_"*80)
-    
-    
+    print("_" * 80)
+
+
 def test():
     example_data = (
         "two1nine\n"
@@ -109,19 +110,19 @@ def test():
         "7pqrstsixteen\n"
     )
     print("Example data:")
-    print(example_data, end='')
-    
+    print(example_data, end="")
+
     lines = example_data.splitlines()
     result = sum_calibration_values(lines)
     expected = 281
     print(f"{result=} | {expected=}")
-    
+
     if result == expected:
         print("TEST PASSED")
     else:
         print("TEST FAILED")
         exit(1)
-    print("_"*80)
+    print("_" * 80)
 
 
 if __name__ == "__main__":
